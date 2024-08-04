e23590fdccc1c4678517b43bf541f67d29d069de
Soham Jaiswal <sosweetham@gmail.com>
2024-08-04 08:01:39 +0530

    feat: unary op

--- server/main.go
+++ server/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"log"
+	"net"
 	"strconv"
 
+	pb "github.com/sosweetham/grpc-demo/proto"
 	"github.com/sosweetham/grpc-demo/util"
+	"google.golang.org/grpc"
 )
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
 
 func main() {
 	config, err := util.LoadConfig(".")
@@ -16,7 +23,19 @@ func main() {
 
 	hostAddress := config.Host + ":" + strconv.FormatInt(int64(config.Port), 10)
 
-	// lis, err := net.Listen("tcp", hostAddress)
+	lis, err := net.Listen("tcp", hostAddress)
+
+	if err != nil {
+		log.Fatalf("Failed to start listener: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("Greet Service registered at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start GRPC server: %v", err)
+	}
+
 
 	// fmt.Printf("%+v\n", config)
 }
